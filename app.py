2bbd7111dc044b13c8caf7ab2fdf88a0e4bbca53
IYRH <inesyaelinrojasherta.31@gmail.com>
2025-10-09 14:55:57 -0600

    arreglando lo de entorno virtual

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html')  # muestra tu portal de becas
 
 if __name__ == '__main__':
     app.run(debug=True)
