5e140da66470c181c6821fedfe6a985e507af7a3
IYRH <inesyaelinrojasherta.31@gmail.com>
2025-10-15 13:01:25 -0600

    poniendo comentarios para saber cuales son los indices

--- database/db_setup.py
+++ database/db_setup.py
@@ -7,22 +7,22 @@ def crear_base_datos():
     # Tabla de solicitudes
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS solicitudes (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,                 
-            nombre TEXT NOT NULL,                   
-            apellidos TEXT NOT NULL,            
-            matricula TEXT NOT NULL,        
-            promedio REAL,              
-            curp TEXT NOT NULL,         
-            correo TEXT NOT NULL,       
-            telefono TEXT,      
-            nss TEXT,       
-            porcentaje_cursado INTEGER,     
-            carrera TEXT,       
-            beca TEXT,     
-            pdf TEXT,       
-            fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP,     
-            estatus TEXT DEFAULT 'Recibida',       
-            comentario_admin TEXT      
+            id INTEGER PRIMARY KEY AUTOINCREMENT,               <!-- s[0] -->
+            nombre TEXT NOT NULL,                               <!-- s[1] -->
+            apellidos TEXT NOT NULL,                            <!-- s[2] --> 
+            matricula TEXT NOT NULL,                            <!-- s[3] -->
+            promedio REAL,                                      <!-- s[4] -->
+            curp TEXT NOT NULL,                                 <!-- s[5] -->
+            correo TEXT NOT NULL,                               <!-- s[6] -->
+            telefono TEXT,                                      <!-- s[7] -->
+            nss TEXT,                                           <!-- s[8] -->
+            porcentaje_cursado INTEGER,                         <!-- s[9] -->
+            carrera TEXT,                                       <!-- s[10] -->
+            beca TEXT,                                          <!-- s[11] -->
+            pdf TEXT,                                           <!-- s[12] -->
+            fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP,  <!-- s[13] -->
+            estatus TEXT DEFAULT 'Recibida',                    <!-- s[14] -->
+            comentario_admin TEXT                               <!-- s[15] -->
         )
     ''')
 
